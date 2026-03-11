a00763548d0d2b85bd51d304b67ee47183aa54ac
Mayel <smayel.dev@gmail.com>
2026-03-11 00:05:42 -0500

    fix cors placed correctly at the start of the function

--- src/main.ts
+++ src/main.ts
@@ -6,38 +6,26 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  //Habilitamos las llamadas a nuestra API desde el frontend
-  // Tiene que ir al principio para que se aplique a todas las rutas, incluyendo Swagger
-
   app.enableCors({
-    origin: 'http://localhost:4200',
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: ['http://localhost:4200', 'https://our-distance-app.vercel.app'],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
+    credentials: true,
   });
 
-  // Swagger
   const config = new DocumentBuilder()
     .setTitle('Our Distance API')
     .setDescription('API para la app de pareja Our Distance')
     .setVersion('1.0')
-    .addBearerAuth() // 👈 habilita el botón de autenticación
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document); // 👈 disponible en /api
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
-
-  //Habilitamos las llamadas a nuestra API desde el frontend
-
-  app.enableCors({
-    origin: ['http://localhost:4200', 'https://our-distance-app.vercel.app'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Accept, Authorization',
-    credentials: true,
-  });
 }
 bootstrap();
